99df38f6ef9b5fc23ccdb2996f2a18488509d763
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-26 22:15:57 +0530

    greetd

--- hosts/system_apps/greetd.nix
+++ hosts/system_apps/greetd.nix
@@ -5,8 +5,24 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
         user = "greeter";
+        command = ''
+          ${pkgs.tuigreet}/bin/tuigreet \
+            --time \
+            --remember \
+            --remember-user-session \
+            --asterisks \
+            --user-menu \
+            --greeting "Welcome to PredatorNix 🚀" \
+            --theme border=cyan \
+            --theme prompt=green \
+            --theme text=white \
+            --theme time=yellow \
+            --theme input=bright-blue \
+            --theme error=red \
+            --theme selected=magenta \
+            --cmd Hyprland
+        '';
       };
     };
   };
