765660f115c0373269d81b4cbdf0edf3c8aa39e4
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-26 22:08:19 +0530

    greetd

--- hosts/system_apps/greetd.nix
+++ hosts/system_apps/greetd.nix
@@ -1,16 +1,17 @@
-
-
 { config, pkgs, ... }:
 {
+  services.greetd = {
+    enable = true;
+  };
 
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "tuigreet --time --remember --cmd Hyprland";
-          user = "greeter";
-        };
+  programs.tuigreet = {
+    enable = true;
+    settings = {
+      default_session = {
+      command = "tuigreet --time --remember --cmd Hyprland";
+      user = "greeter";
       };
     };
+  };
 }
 
