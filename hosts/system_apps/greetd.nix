57530a4263f68cf2fdbea471795145e89b702060
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-08-29 14:46:42 +0530

    tty colors changed + greetd colors updated

--- hosts/system_apps/greetd.nix
+++ hosts/system_apps/greetd.nix
@@ -1,15 +1,27 @@
-{ config, pkgs, ... }:
+# { config, pkgs, ... }:
+#
+# {
+#   services.greetd = {
+#     enable = true;
+#     settings = {
+#       default_session = {
+#         command =
+#           "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '  Welcome to PredatorNix  ' --theme prompt=green text=yellow time=yellow input=bright-blue error=red selected=magenta --cmd Hyprland";
+#         user = "greeter";
+#       };
+#     };
+#   };
+# }
 
-{
+{ config, pkgs, ... }: {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         command =
-          "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '  Welcome to PredatorNix  ' --theme prompt=green text=white time=yellow input=bright-blue error=red selected=magenta --cmd Hyprland";
+          "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd Hyprland";
         user = "greeter";
       };
     };
   };
 }
-
--- hosts/system_apps/user.nix
+++ hosts/system_apps/user.nix
@@ -66,23 +66,41 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-
-    # package = inputs.hyprland.packages."$(pkgs.system)".hyprland; # hyprland-packages enabling?
   };
 
-  # fonts.packages = with pkgs; [
-  #   dejavu_fonts
-  #   noto-fonts
-  #   noto-fonts-cjk-sans
-  #   noto-fonts-emoji
-  #   nerd-fonts.fira-code
-  #   nerd-fonts.jetbrains-mono
-  #   nerd-fonts.hack
-  # ];
+  # console = {
+  #   font = "Lat2-Terminus16"; # good default
+  #   packages = with pkgs; [ terminus_font ];
+  # };
 
   console = {
-    font = "Lat2-Terminus16"; # good default
+    # For smaller fonts, try these options:
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-112n.psf.gz"; # Small
+    # font = "${pkgs.terminus_font}/share/consolefonts/ter-116n.psf.gz";  # Medium-small
+    # font = "${pkgs.terminus_font}/share/consolefonts/ter-120n.psf.gz";  # Medium
+    # font = "${pkgs.terminus_font}/share/consolefonts/ter-124n.psf.gz";  # Medium-large
+    # font = "${pkgs.terminus_font}/share/consolefonts/ter-128n.psf.gz";  # Large
+    # font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";  # Extra large (what you had)
     packages = with pkgs; [ terminus_font ];
+    colors = [
+      # Define 16 colors for better TTY experience
+      "1e1e2e" # base
+      "f38ba8" # red
+      "a6e3a1" # green
+      "f9e2af" # yellow
+      "89b4fa" # blue
+      "cba6f7" # magenta
+      "94e2d5" # cyan
+      "bac2de" # white
+      "45475a" # bright black
+      "f38ba8" # bright red
+      "a6e3a1" # bright green
+      "f9e2af" # bright yellow
+      "89b4fa" # bright blue
+      "cba6f7" # bright magenta
+      "94e2d5" # bright cyan
+      "ffffff" # bright white
+    ];
   };
 }
 
