c1ee9892ed0003fe7649aecd59d8973301e1bf51
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-11 13:25:35 +0530

    remove old styles

--- hosts/apps/rofi/default.nix
+++ hosts/apps/rofi/default.nix
@@ -1,13 +1,15 @@
 # Y: Wayland app launcher.
-{
+
+{ lib, config, pkgs ... }: {
+
   programs.rofi = {
     enable = true;
-    # theme = ./config.rasi; # this field shoule have theme name
-    themeFile = ./config.rasi;
+    theme = ./config.rasi; # this field shoule have theme name
+    # themeFile = ./config.rasi;
 
   };
 
-  # home.file.".config/rofi/config.rasi".source = ./config.rasi; # Y: the above programs.rofi.theme dose the same thing.
+  home.file.".config/rofi/config.rasi".source = ./config.rasi; # Y: the above programs.rofi.theme dose the same thing.
   home.file.".config/rofi/shared/colors.rasi".source = ./colors.rasi;
   home.file.".config/rofi/shared/fonts.rasi".source = ./fonts.rasi;
 }
