0439d46bdf784083175d78a3283f82e13d0422d7
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-13 22:18:23 +0530

    done

--- hosts/apps/sessionVariables.nix
+++ hosts/apps/sessionVariables.nix
@@ -6,6 +6,14 @@
     FILEMANAGER = "yazi";
     EDITOR = "nvim";
     CARGO_TARGET_DIR = "${config.home.homeDirectory}/.cargo/target";
+
+    # # Wayland-specific Y: IMPUT METHODS 
+    # GTK_IM_MODULE = "fcitx";
+    # QT_IM_MODULE = "fcitx";
+    # XMODIFIERS = "@im=fcitx";
+    # SDL_IM_MODULE = "fcitx";
+    # # Ensure Wayland compatibility
+    # GLFW_IM_MODULE = "ibus";
   };
 
 }
