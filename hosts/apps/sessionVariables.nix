ea7927f94b7141f36f1b82a483f36f54b15b81dd
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-15 18:42:45 +0530

    session var chaged

--- hosts/apps/sessionVariables.nix
+++ hosts/apps/sessionVariables.nix
@@ -9,8 +9,8 @@
 
     # # Wayland-specific Y: IMPUT METHODS 
     INPUT_METHOD = "fcitx";
-    GTK_IM_MODULE = ""; # unset, let Wayland IM handle
-    QT_IM_MODULE = ""; # unset, let Wayland IM handle
+    GTK_IM_MODULE = "wayland"; # unset, let Wayland IM handle
+    QT_IM_MODULE = "wayland"; # unset, let Wayland IM handle
     XMODIFIERS = "@im=fcitx";
     GLFW_IM_MODULE = "ibus"; # optional, some games/toolkits expect this
   };
