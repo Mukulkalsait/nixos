8ff7259ac038a2f96fbdc9cb252d1c79afc33f27
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-15 18:29:52 +0530

    usser config

--- hosts/system_apps/user.nix
+++ hosts/system_apps/user.nix
@@ -21,23 +21,5 @@
     options = [ "uid=1000" "gid=100" "umask=0022" ];
   };
 
-  i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-m17n # transliteration (Hindi/Marathi)
-      fcitx5-gtk # GTK apps (WPS, Firefox, etc.)
-    ];
-  };
-
-  # R: this above service is very important for marathi/hindi typing with EHGLIS keyboard.
-  # its called as phonetic Typing.
-  # we are using m17n engine to do this. 
-  # Y: Configure.
-  # Go to IBus Preferences → Input Method → Add → m17n:hi-transliteration (for Hindi) or m17n:mr-transliteration (for Marathi).
-  # Now when you switch input method (usually Ctrl+Space), you can type like this:
-  # Type: namaste → Output: नमस्ते
-  # Type: bharat → Output: भारत
-  # This works system-wide, including WPS Office.
-
 }
 
