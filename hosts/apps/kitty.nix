c1079e125bcbd6087937bf3290bb171598326707
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-23 17:26:14 +0530

    kitty upgraded + zellij Ctrl+Shift+q

--- hosts/apps/kitty.nix
+++ hosts/apps/kitty.nix
@@ -14,13 +14,15 @@
       background = "#000000";
       background_opacity = "0.95";
       confirm_os_window_close = 0;
-      cursor_trail = "1";
-      cursor_trail_decay = "0.25 0.5"; # X and Y decay
-      cursor_trail_thickness = "1.0 1.0"; # X and Y thickness
+      cursor_shape = "beam";
+      cursor_trail = "3";
+      cursor_trail_decay = "0.25 0.3"; # X and Y decay
+      cursor_trail_thickness = "1.2 1.2"; # X and Y thickness
     };
     keybindings = {
       "ctrl+shift+c" = "copy_to_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
+      "ctrl+shift+q" = "no_op"; # unbind close_tab
     };
   };
 
