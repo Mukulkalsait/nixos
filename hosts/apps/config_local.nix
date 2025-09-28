8d15e9e7775a7c43b860977ba53b61ff4dd6512e
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-28 22:05:04 +0530

    Updates in config

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -1,5 +1,5 @@
 {
-  # home.file.".config/zellij".source = ./.config_local/zellij;
+  home.file.".config/zellij".source = ./.config_local/zellij;
   home.file.".config/fastfetch".source = ./.config_local/fastfetch;
 }
 
