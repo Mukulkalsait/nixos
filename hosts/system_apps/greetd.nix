015d6494c13b06e645461e97afb38ba00ac30abe
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-01 11:34:38 +0530

    greeted updated but working model kept juist in case

--- hosts/system_apps/greetd.nix
+++ hosts/system_apps/greetd.nix
@@ -1,11 +1,22 @@
 # Y: TUI Login manager.
 { config, pkgs, ... }: {
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command =
+  #         "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd Hyprland";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
+
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         command =
-          "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd Hyprland";
+          "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd Hyprland";
         user = "greeter";
       };
     };
