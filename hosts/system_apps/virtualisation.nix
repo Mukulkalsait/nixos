2c426f3104d4db6faf14f9a2bbcb9ea7163f890a
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-06 18:21:17 +0530

    Updated in virtulisation nix

--- hosts/system_apps/virtualisation.nix
+++ hosts/system_apps/virtualisation.nix
@@ -8,7 +8,8 @@
 
   users.users.mukuldk.extraGroups = [ "docker" ]; # group add to user.
 
-  # Y: (Optional) Podman alongside Docker.
+  # Y: (Optional) Podman alongside Docker will start podman soon.
+  # DX:
   # virtualisation.podman.enable = true;
   # virtualisation.podman.enableOnBoot = false;
   # virtualisation.podman.dockerCompat = true;
