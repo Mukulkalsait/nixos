e80ee98d5a7f6832c64336851067f3dbf1fc6bad
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-09 16:56:27 +0530

    rust c path added

--- hosts/system_apps/envVariables.nix
+++ hosts/system_apps/envVariables.nix
@@ -3,7 +3,8 @@
   environment.variables = {
     CC = "${pkgs.gcc}/bin/gcc";
     LD = "${pkgs.lld}/bin/ld.lld";
-    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/library"; # Use rustPlatform.rustLibSrc for std source
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+    # RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/library"; # Use rustPlatform.rustLibSrc for std source
     # ... other variables
   };
 }
