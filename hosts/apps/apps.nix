9ddc05bf8c76ac05d9d48f87407f26e0440443d8
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-13 17:55:30 +0530

    Updated with DAW Aurdor.

--- hosts/apps/apps.nix
+++ hosts/apps/apps.nix
@@ -75,6 +75,7 @@
     libreoffice-fresh # community driven latest version fo libreoffice.
     obs-studio
     vlc
+    ardour
     graphviz # graph vitrulisatoin tool extreamly multypurpose. 
     # davinci-resolve
     # libxcb # for davinci-resovle
