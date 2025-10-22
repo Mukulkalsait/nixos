8c5ac2d68b6cd6d20bd86b9383f4d60354a87a43
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-22 20:34:34 +0530

    Updates

--- hosts/system_apps/linuwu-sense.nix
+++ hosts/system_apps/linuwu-sense.nix
@@ -1,11 +1,12 @@
 # Y: PREDATOR Sense Solution In NixOs
 
-{ pkgs, lib, config, ... }:
+# Y: PREDATOR Sense Solution In NixOS
+{ pkgs, lib, ... }:
 
 let
-  kernel = config.boot.kernelPackages.kernel;
-  kernelPackages = config.boot.kernelPackages;
-  linuwu-sense = pkgs.stdenv.mkDerivation {
+  kernel = pkgs.linuxPackages_latest.kernel;
+
+  linuwu-sense = pkgs.stdenv.mkDerivation rec {
     pname = "linuwu-sense";
     version = "unstable-2025-09-06";
 
@@ -19,18 +20,28 @@ let
     nativeBuildInputs = with pkgs; [ pkg-config kmod gcc gnumake ];
     buildInputs = [ kernel.dev ];
 
-    makeFlags = [
-      "KERNELDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    ];
+    # Ensure the Makefile never tries to use /lib/modules paths
+    postPatch = ''
+      sed -i "s|/lib/modules/\\$(KVER)/build|${kernel.dev}/lib/modules/${kernel.modDirVersion}/build|g" Makefile
+      sed -i "s|/lib/modules/\\$(shell uname -r)/build|${kernel.dev}/lib/modules/${kernel.modDirVersion}/build|g" Makefile
+    '';
+
+    buildPhase = ''
+      make KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build \
+           KERNELDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build \
+           M=$(pwd) modules
+    '';
 
     installPhase = ''
-      install -D src/linuwu_sense.ko $out/lib/modules/${kernel.modDirVersion}/extra/linuwu_sense.ko
+      mkdir -p $out/lib/modules/${kernel.modDirVersion}/extra
+      install -Dm644 src/linuwu_sense.ko $out/lib/modules/${kernel.modDirVersion}/extra/linuwu_sense.ko
     '';
 
     meta = with lib; {
       description = "Kernel module for Acer Predator fan/RGB/turbo/battery control";
       license = licenses.gpl2Only;
       platforms = platforms.linux;
+      maintainers = [ maintainers.yourName or "mukuldk" ];
     };
   };
 in
@@ -38,5 +49,26 @@ in
   boot.extraModulePackages = [ linuwu-sense ];
   boot.kernelModules = [ "linuwu_sense" ];
   boot.blacklistedKernelModules = [ "acer_wmi" ];
+
+  systemd.services.linuwu-sense-setup = {
+    description = "Apply Linuwu-Sense tweaks (RGB + Battery limiter)";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sys-module-linuwu_sense.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = [
+        # Set permissions
+        "${pkgs.findutils}/bin/find /sys/module/linuwu_sense/drivers/platform:acer-wmi/acer-wmi/predator_sense/ -type f -exec chmod 660 {} +"
+        "${pkgs.findutils}/bin/find /sys/module/linuwu_sense/drivers/platform:acer-wmi/acer-wmi/predator_sense/ -type f -exec chgrp wheel {} +"
+
+        # Keyboard RGB mode
+        "${pkgs.bash}/bin/sh -c 'echo \"3,1,100,2,0,0,0\" | ${pkgs.coreutils}/bin/tee /sys/module/linuwu_sense/drivers/platform:acer-wmi/acer-wmi/four_zoned_kb/four_zone_mode'"
+
+        # Battery limiter
+        "${pkgs.bash}/bin/sh -c 'echo \"1\" | ${pkgs.coreutils}/bin/tee /sys/module/linuwu_sense/drivers/platform:acer-wmi/acer-wmi/predator_sense/battery_limiter'"
+      ];
+    };
+  };
 }
 
