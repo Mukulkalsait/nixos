6e8f87e592caed954e187f52b777c4aca8ab764d
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-12-11 23:41:52 +0530

    rust path repaired

--- hosts/system_apps/envVariables.nix
+++ hosts/system_apps/envVariables.nix
@@ -3,8 +3,8 @@
   environment.variables = {
     CC = "${pkgs.gcc}/bin/gcc";
     LD = "${pkgs.lld}/bin/ld.lld";
-    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-    # RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/library"; # Use rustPlatform.rustLibSrc for std source
+    # RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/library"; # <-- UNCOMMENT/USE THIS (correct for std sources)
     # ... other variables
   };
 }
