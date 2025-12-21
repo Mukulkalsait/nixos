dda6c25a7982086cc35a940ef067775b55a30e9e
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-12-21 23:22:24 +0530

    Mangohud added fully

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -1,5 +1,6 @@
 {
   home.file.".config/zellij".source = ./.config_local/zellij;
   home.file.".config/fastfetch".source = ./.config_local/fastfetch;
+  home.file.".config/MangoHud".source = ./.config_local/MangoHud;
 }
 
