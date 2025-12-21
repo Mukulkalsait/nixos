fa689dc38fa0e5685115439c2952d64379635297
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-12-21 23:27:05 +0530

    config ghostty added

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -2,5 +2,6 @@
   home.file.".config/zellij".source = ./.config_local/zellij;
   home.file.".config/fastfetch".source = ./.config_local/fastfetch;
   home.file.".config/MangoHud".source = ./.config_local/MangoHud;
+  home.file.".config/ghostty".source = ./.config_local/ghostty;
 }
 
