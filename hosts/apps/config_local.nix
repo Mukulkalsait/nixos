e2a978865b95a666cc1818c0a5e57e0547a66740
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-12-22 17:59:24 +0530

    local nix

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -1,9 +1,11 @@
 {
-  home.file.".config/zellij".source = ./.config_local/zellij;
-  home.file.".config/fastfetch".source = ./.config_local/fastfetch;
-  home.file.".config/MangoHud".source = ./.config_local/MangoHud;
-  home.file.".config/ghostty".source = ./.config_local/ghostty;
-  home.file.".config_local/ghostty/shaders".source = ./.config_local/ghostty/shaders;
-  # home.file.".config_local/ghostty/shaders".source = ./.config_local/ghostty/shaders;
+  home.file.".config/zellij".source = ./config_local/zellij;
+  home.file.".config/fastfetch".source = ./config_local/fastfetch;
+  home.file.".config/MangoHud".source = ./config_local/MangoHud;
+  home.file.".config/ghostty".source = ./config_local/ghostty;
+  home.file.".config_local/ghostty/shaders".source = ./config_local/ghostty/shaders;
+  # home.file.".config_local/ghostty/shaders".source = ./config_local/ghostty/shaders;
+
+
 }
 
