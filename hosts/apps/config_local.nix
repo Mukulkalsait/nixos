aae7983e7c9b6b79c601b9722033f812bbd85295
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-12-22 17:54:33 +0530

    Updates

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -3,5 +3,6 @@
   home.file.".config/fastfetch".source = ./.config_local/fastfetch;
   home.file.".config/MangoHud".source = ./.config_local/MangoHud;
   home.file.".config/ghostty".source = ./.config_local/ghostty;
+  home.file.".config_local/ghostty/shaders".source = ./.config_local/ghostty/shaders;
 }
 
