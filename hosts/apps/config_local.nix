dc82fb91cdb49f89bb1e58167b747e6209fde175
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-12-22 18:05:45 +0530

    Updates

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -1,11 +1,7 @@
 {
-  home.file.".config/zellij".source = ./config_local/zellij;
-  home.file.".config/fastfetch".source = ./config_local/fastfetch;
-  home.file.".config/MangoHud".source = ./config_local/MangoHud;
-  home.file.".config/ghostty".source = ./config_local/ghostty;
-  home.file.".config_local/ghostty/shaders".source = ./config_local/ghostty/shaders;
-  # home.file.".config_local/ghostty/shaders".source = ./config_local/ghostty/shaders;
-
-
+  home.file.".config/zellij".source = ./config_local_folder/zellij;
+  home.file.".config/fastfetch".source = ./config_local_folder/fastfetch;
+  home.file.".config/MangoHud".source = ./config_local_folder/MangoHud;
+  home.file.".config/ghostty".source = ./config_local_folder/ghostty;
 }
 
