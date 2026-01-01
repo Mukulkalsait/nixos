4c360846b7d918516801f2a883ba04e752cce5f7
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-01 23:14:09 +0530

    Updates

--- flake.nix
+++ flake.nix
@@ -44,14 +44,16 @@
 
   };
 
-  nixConfig = {
-    extra-substituters = [
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
     ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8wA5R7Zty6U/3+VNRG0cL1wEw="
-      "hyprland.cachix.org-1:8F8mQAFN2wVYyTn+zBPRK6ZKqx0fP5cH3gX2rT9C/Rw="
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 
