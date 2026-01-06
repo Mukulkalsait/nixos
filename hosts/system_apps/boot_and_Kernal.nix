7d49e20bd598f5f80eead450adab87aa52fe13a0
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-06 17:25:32 +0530

    boot kernal zswqp removed as we are already on zram which is better for our system

--- hosts/system_apps/boot_and_Kernal.nix
+++ hosts/system_apps/boot_and_Kernal.nix
@@ -9,9 +9,9 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest; # latest Kernal Pkg. # DX: kernal selector but its inside Linuvu-sense*.nix
 
-  boot.extraModulePackages = [
-    pkgs.linuxKernel.packages.linux_6_18.acer-wmi-battery # B: Acer kernal module for 6.18 => (⭐) 
-  ];
+  # boot.extraModulePackages = [
+  #   pkgs.linuxKernel.packages.linux_6_18.acer-wmi-battery # B: Acer kernal module for 6.18 => (⭐) 
+  # ];
 
   boot.kernelModules = [
     # Y:  Enable Kernal MODULES
@@ -23,11 +23,13 @@
   ];
   boot.kernelParams = [
     "acpi_ec.gpe_debug=1" # Enable Kernal parameters
-    "zswap.enabled=1" # z-swap on
-    "zswap.compressor=lz4" # compressor type = LZ4 => best
-    "zswap.zpool=z3fold" # Efficient memory packing
-    "zswap.max_pool_percent=20" # Caps => limit 20% VVVIMP
     "preempt=full" # Enamble PREE_MTP=LOW LETENCY FOR MUSIC PRODUCITON => PREMTP_RT (too poweroful for us.)
+
+    # Y: we dont need zswap because we hve zram which is better for persnol systems. so NO ZSWAP
+    # "zswap.enabled=1" # z-swap on
+    # "zswap.compressor=lz4" # compressor type = LZ4 => best
+    # "zswap.zpool=z3fold" # Efficient memory packing
+    # "zswap.max_pool_percent=20" # Caps => limit 20% VVVIMP
   ];
 
 }
