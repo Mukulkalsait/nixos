0312ad5782d7d0175352b2ef21e5d93c44cb3905
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-08 19:33:39 +0530

    battery indicator updated but did not owrke at all.

--- hosts/apps/hyprland/default.nix
+++ hosts/apps/hyprland/default.nix
@@ -1,3 +1,14 @@
 {
-  imports = [ ./binds.nix ./hypridle.nix ./hyprlock.nix ./swww.nix ./main.nix ];
+  imports = [
+    ./binds.nix
+    ./hypridle.nix
+    ./hyprlock.nix
+    ./swww.nix
+    ./main.nix
+  ];
+
+  # Hyprland-Internal_configs.
+  home.file.".config/hypr/media".source = ./media;
+  home.file.".config/hypr/scripts".source = ./scripts;
+
 }
