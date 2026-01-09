482ac37b358b921e9d28a94532d00cb8fb49d518
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-09 17:51:22 +0530

    sc-card new addtions flags

--- hosts/system_apps/user.nix
+++ hosts/system_apps/user.nix
@@ -17,19 +17,19 @@
   };
 
   # IMP: SD MOUNNTING PERMENANT
-  fileSystems."/home/mukuldk/1_file/8_MOUNTINGS/SD_512" = {
-    device = "UUID=9C33-6BBD";
-    fsType = "exfat";
-    options = [
-      "uid=1000"
-      "gid=100"
-      "umask=0022"
-      "nofail" # System boots even if SD is missing
-      "x-systemd.automount" # Mounts only when accessed
-      "x-systemd.device-timeout=5s" # timeout = 5s (default 90s)
-      "noatime" # Stops updating “last accessed” timestamps
-    ];
-  };
+  # fileSystems."/home/mukuldk/1_file/8_MOUNTINGS/SD_512" = {
+  #   device = "UUID=9C33-6BBD";
+  #   fsType = "exfat";
+  #   options = [
+  #     "uid=1000"
+  #     "gid=100"
+  #     "umask=0022"
+  #     "nofail" # System boots even if SD is missing
+  #     "x-systemd.automount" # Mounts only when accessed
+  #     "x-systemd.device-timeout=5s" # timeout = 5s (default 90s)
+  #     "noatime" # Stops updating “last accessed” timestamps
+  #   ];
+  # };
 
 
   # script = ''
