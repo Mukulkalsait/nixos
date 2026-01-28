3f63e2ba039534cf56052651ea6fa1d147c34556
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-28 12:32:25 +0530

    zsh cmd

--- hosts/apps/zsh/default.nix
+++ hosts/apps/zsh/default.nix
@@ -14,7 +14,7 @@
     history.size = 100000;
     history.path = "${config.xdg.dataHome}/zsh/history";
 
-    # Y: Commands to run as soon as zsh loaded 🔻 
+    # Y: Commands to run as soon as zsh loaded 🔻
     # initExtra = '' source ~/.config/zsh/scripts/y.sh '';# B: does same but we slightly extra works. so we dont need it..
     initContent =
       "source ~/.config/zsh/scripts/y.sh "; # B: we are sourcing file we defined at the top of this config.
