2150bf07c756fc93ad09d5609dcafb20f7894586
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-30 21:23:07 +0530

    Starship seperate for root

--- hosts/system_apps/starship/default.nix
+++ hosts/system_apps/starship/default.nix
@@ -2,7 +2,7 @@
 { config, pkgs, ... }: {
   programs.starship = {
     enable = true;
-    settings = pkgs.lib.importTOML ./starship-tokyonight.toml.toml;
+    settings = pkgs.lib.importTOML ./starship-tokyonight.toml;
   };
 }
 
