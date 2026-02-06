6501fccf80cb4927e8668d9a26958a120f858a5d
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-06 15:24:01 +0530

    changed some battery problemetic things

--- hosts/system_apps/hardware_and_services.nix
+++ hosts/system_apps/hardware_and_services.nix
@@ -1,11 +1,9 @@
 # Y: Hardware Services
 { pkgs, ... }: {
-
   # G:  Networking and DNS resolver.
-  services.resolved = {
-    enable = true; # This gives Local stub dns-resolver at 127.0.0.53 by systemd
-    # R: Local DNS stub is => application{ Local library of DNS+IP }=> that saves time + cpu + RECURSOVE-RESOLVATION ⭐ in local mashine.
-  };
+  services.resolved = { enable = true; };
+  # R: Local DNS stub is => application{ Local library of DNS+IP }=> that saves time + cpu + RECURSOVE-RESOLVATION ⭐ in local mashine.
+
   environment.etc."systemd/resolved.conf.d/00-custom.conf".text = ''
     [Resolve]
     DNSStubListener=yes
@@ -14,54 +12,77 @@
   '';
 
   networking = {
-    hostName = "PredatorNix"; # Define your hostname.
-    enableIPv6 = true; # newly added
+    hostName = "PredatorNix"; # our Hostname
+    enableIPv6 = true;
     networkmanager.enable = true;
-    networkmanager.dns = "systemd-resolved"; # For custome DNS=> by systemd.resolver 👇
+    networkmanager.dns = "systemd-resolved"; # CostumeDNS => systemd.resolver
     nameservers = [
-      "1.1.1.1" # Cloudflare
+      "1.1.1.1" # Cloudfare
       "1.0.0.1"
-      "8.8.8.8" # Google (fallback)
+      "8.8.8.8" # google (fallback)
       "8.8.4.4"
     ];
   };
-  # ----------------------------
-  #
-  # G:  Bluetooth.
-  hardware.bluetooth.enable = true; # Enable Bluetooth
-  services.blueman.enable = true; # optional: nice GTK tray app
+
+  # G: Bluetooth.
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true; # gtx tray app.
 
   # G: Sensors.
-  hardware.i2c.enable = true; # Enable sensors.
+  hardware.i2c.enable = true; # enable sensors.
 
   # Y: THUNDERBOLT
-  services.hardware.bolt.enable = true; # Bolt daemon:Thunderbolt
-  services.udev.packages =
-    [ pkgs.bolt ]; # Optional:udevRules BetterDeviceHandling.
+  services.hardware.bolt.enable = true; #BOLT daemon Thunderbold
+  services.udev.packages = [ pkgs.bolt ]; #UDEV.Rules battery device handling.
 
-  # Y: Battery Indicator may be.
+  # Y: POWER MANAGEMENT - FIXED
   powerManagement.enable = true;
-  services.power-profiles-daemon.enable = true; # better Thunderbolt power management
-  # 👆🏻 thunderbolt or powerprofilectl command. => or use "tlp"
+
+  # DX: REMOVE this - it conflicts with proper battery management
+  # services.power-profiles-daemon.enable = true;  # REMOVE THIS LINE
+
+  # Use TLP instead for better power management
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Battery thresholds (won't work on your hardware, but doesn't hurt)
+      START_CHARGE_THRESH_BAT1 = 75;
+      STOP_CHARGE_THRESH_BAT1 = 80;
+
+      # CPU settings
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      # Enable audio power saving
+      SOUND_POWER_SAVE_ON_AC = 0;
+      SOUND_POWER_SAVE_ON_BAT = 1;
+    };
+  };
+
   services.dbus.enable = true;
+
+  # UPower configuration - CORRECTED
   services.upower = {
     enable = true;
     percentageLow = 20;
     percentageCritical = 10;
     percentageAction = 5;
-    criticalPowerAction = "HybridSleep";
-  };
-  services.logind = {
-    lidSwitch = "suspend";
-    settings.Login = {
-      HandlePowerKey = "ignore";
-      HandleSuspendKey = "suspend";
-      HandleHibernateKey = "hibernate";
-      HandleLidSwitch = "suspend";
-      HandleLidSwitchExternalPower = "ignore";
-    };
+    criticalPowerAction = "PowerOff"; # Changed to PowerOff for reliability
   };
 
+  # Logind settings
+  services.logind = {
+    lidSwitch = "suspend";
+    lidSwitchDocked = "ignore";
+    lidSwitchExternalPower = "ignore";
+    extraConfig = ''
+      HandlePowerKey=ignore
+      HandleSuspendKey=suspend
+      HandleHibernateKey=hibernate
+      IdleAction=ignore
+      IdleActionSec=30min
+    '';
+  };
 
   # Y:  Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true; 
@@ -70,12 +91,9 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    # alsa.support32bit = true; # does not exisists;
     pulse.enable = true;
     jack.enable = true;
   };
 
-  services.printing.enable = false; # Y: CUPS => print documents.
-  # services.modemmanager.enable = false; Y: variable not avialable error.
-}
-
+  services.printing.enable = false;
+} 
