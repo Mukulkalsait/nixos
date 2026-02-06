4ad1c08129a801840becae3210a0b4bcb2348a02
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-06 16:09:24 +0530

    Updates

--- hosts/apps/swaync/scripts/battery-warning.sh
+++ hosts/apps/swaync/scripts/battery-warning.sh
@@ -5,10 +5,12 @@ BATTERY_STATUS=$(cat /sys/class/power_supply/BAT1/status)
 
 if [ "$BATTERY_STATUS" = "Discharging" ]; then
   if [ "$BATTERY_LEVEL" -le 5 ]; then
-    notify-send -a "battery" -u critical -t 0 -i battery-caution "Battery Critical!" "Only ${BATTERY_LEVEL}% remaining!\nSystem will shutdown soon!"
+    notify-send -a "battery" -u critical -t 0 -i battery-caution "Battery Critical! 🪫" "Only ${BATTERY_LEVEL}% remaining!\nSystem will shutdown soon!"
   elif [ "$BATTERY_LEVEL" -le 10 ]; then
-    notify-send -a "battery" -u critical -i battery-low "Battery Low!" "${BATTERY_LEVEL}% remaining\nPlease plug in charger!"
+    notify-send -a "battery" -u critical -i battery-low "Battery Super Low! 🪫" "${BATTERY_LEVEL}% remaining\nPlease plug in charger!"
   elif [ "$BATTERY_LEVEL" -le 20 ]; then
-    notify-send -a "battery" -u normal -i battery-low "Battery Low" "${BATTERY_LEVEL}% remaining"
+    notify-send -a "battery" -u normal -i battery-low "Battery Low ⚠️" "${BATTERY_LEVEL}% remaining"
+  else
+    notify-send -a "battery" -u normal -i battery-low "Battery is working" "${BATTERY_LEVEL}% ⚡"
   fi
 fi
