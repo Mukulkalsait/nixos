bdcac0f6f671ad5d3998b295043d0f5777298239
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-13 13:11:27 +0530

    Defualt

--- hosts/apps/hyprland/scripts/floating_audio.sh
+++ hosts/apps/hyprland/scripts/floating_audio.sh
@@ -2,9 +2,9 @@
 
 # If impala exists → kill it
 if hyprctl clients | grep -q "class: wiremix"; then
-  pkill -f "kitty --class wiremix"
+  pkill -f "kitty --class=\"wiremix\""
 else
-  kitty --class="wiremix" title="WireMix-Audio 📢 " -e wiremix &
+  kitty --class="wiremix" --title="WireMix-Audio 📢 " -e wiremix &
   sleep 0.07
   hyprctl dispatch resizeactive exact 28% 65%
   hyprctl dispatch moveactive exact 15% 3%
