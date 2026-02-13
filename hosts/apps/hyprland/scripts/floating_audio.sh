0b1a04c90fc770833065e036855cd005ba5fb049
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-13 13:47:37 +0530

    Updates

--- hosts/apps/hyprland/scripts/floating_audio.sh
+++ hosts/apps/hyprland/scripts/floating_audio.sh
@@ -2,11 +2,10 @@
 
 # If impala exists → kill it
 if hyprctl clients | grep -q "class: wiremix"; then
-  # hyprctl dispatch closewindow class:wiremix &&
-  pkill -f "kitty --class=wiremix"
+  hyprctl dispatch closewindow class:wiremix || pkill -f "kitty --class=wiremix"
 else
-  kitty --class="wiremix" --title="WireMix-Audio 📢 " -e wiremix &
-  sleep 0.07
-  hyprctl dispatch resizeactive exact 28% 65%
-  hyprctl dispatch moveactive exact 15% 3%
+  kitty --class="wiremix" --title="WireMix-Audio 📢 " -o background_opacity=1.0 -o background=#1e1e2e -e wiremix -v output &
+  sleep 0.1
+  hyprctl dispatch resizeactive exact 58% 45%
+  hyprctl dispatch moveactive exact 2% 3%
 fi
--- hosts/apps/hyprland/scripts/floating_network.sh
+++ hosts/apps/hyprland/scripts/floating_network.sh
@@ -2,11 +2,10 @@
 
 # If impala exists → kill it
 if hyprctl clients | grep -q "class: nmtui"; then
-  # hyprctl dispatch closewindow class:nmtui &&
-  pkill -f "kitty --class=nmtui"
+  hyprctl dispatch closewindow class:nmtui || pkill -f "kitty --class=nmtui"
 else
   kitty --class="nmtui" --title="NMTUI 󱛆 " -e nmtui &
-  sleep 0.07
+  sleep 0.1
   hyprctl dispatch resizeactive exact 28% 65%
   hyprctl dispatch moveactive exact 62% 3%
 fi
