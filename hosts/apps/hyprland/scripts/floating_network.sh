88a4f3c34cd0d7adbe97fa657382d84867be1d46
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-13 12:37:12 +0530

    Updated

---
+++ hosts/apps/hyprland/scripts/floating_network.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# If impala exists → kill it
+if hyprctl clients | grep -q "class: nmtui"; then
+  pkill -f "kitty --class nmtui"
+else
+  kitty --class nmtui --title="NMTUI 󱛆 " -e nmtui &
+  sleep 0.07
+  hyprctl dispatch resizeactive exact 28% 65%
+  hyprctl dispatch moveactive exact 62% 3%
+fi
