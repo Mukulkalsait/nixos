657fb736cfd9f64bded3c0b1c0867504470368c1
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-13 12:57:09 +0530

    Updated

--- hosts/apps/hyprland/scripts/floating_other.sh
+++ hosts/apps/hyprland/scripts/floating_other.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 # If impala exists → kill it
-# if hyprctl clients | grep -q "class: iwd-impala"; then
-#   pkill -f "kitty --class iwd-impala"
-# else
-#   kitty --class iwd-impala -e impala &
-#   sleep 0.07
-#   hyprctl dispatch resizeactive exact 28% 65%
-#   hyprctl dispatch moveactive exact 72% 3%
-# fi
-
-echo "havnt set yet."
+if hyprctl clients | grep -q "class: wiremix"; then
+  pkill -f "kitty --class wiremix"
+else
+  kitty --class="wiremix" title="WireMix-Audio 📢 " -e wiremix &
+  sleep 0.07
+  hyprctl dispatch resizeactive exact 28% 65%
+  hyprctl dispatch moveactive exact 15% 3%
+fi
