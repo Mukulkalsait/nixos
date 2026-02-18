6eb1c79c775ee762791eab676dd71c80e7a4b6a7
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-18 13:54:24 +0530

    Browser-vm movde to dark-net

---
+++ hosts/microvm/dark-net.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "kamehameha";
+
+  microvm = {
+    hypervisor = "qemu";
+    vcpu = 2; # 2cpu
+    mem = 3072; # 3Gb
+    shares = [ ];
+    volumes = [{
+      mountPoint = "/";
+      image = "root.img";
+      size = 8192; # 8GB
+    }];
+  };
+
+  services.getty.autologinUser = "root";
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.startx.enable = true;
+  services.xserver.windowManager.openbox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    tor-browser
+    xterm
+  ];
+
+}
+
