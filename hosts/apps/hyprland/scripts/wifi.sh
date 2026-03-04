953f0ba4264704dd7ae0fe494bfb804d4a7fa19c
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-03-05 00:04:01 +0530

    Updates

--- hosts/apps/hyprland/scripts/wifi.sh
+++ hosts/apps/hyprland/scripts/wifi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nmcli -t -f WIFI g | grep enabled >/dev/null && echo "󰤨 WiFi" || echo "󰤭 Offline"
+nmcli -t -f WIFI g | grep enabled >/dev/null && echo "󰤨  WiFi" || echo "󰤭  Offline"
