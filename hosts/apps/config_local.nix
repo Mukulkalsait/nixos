cfe138b4ccebc047e0376d322f20bafc989fb164
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-03-10 18:38:13 +0530

    Updated Paths FIxed

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -3,6 +3,8 @@
   home.file.".config/fastfetch".source = ./config_local_folder/fastfetch;
   home.file.".config/MangoHud".source = ./config_local_folder/MangoHud;
   home.file.".config/ghostty".source = ./config_local_folder/ghostty;
+  home.file.".config/gamemode.ini".source = ./config_local_folder/gamemode.ini;
+
   # x
 }
 
