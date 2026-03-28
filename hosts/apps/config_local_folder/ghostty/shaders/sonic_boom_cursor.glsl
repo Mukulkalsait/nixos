a31638e84538564a2a17e7801cf9b38debc1d9b3
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-03-28 18:43:35 +0530

    updates

--- hosts/apps/config_local_folder/ghostty/shaders/sonic_boom_cursor.glsl
+++ hosts/apps/config_local_folder/ghostty/shaders/sonic_boom_cursor.glsl
@@ -1,10 +1,11 @@
 // CONFIGURATION
 const float DURATION = 0.100;               // How long the ripple animates (seconds)
-const float MAX_RADIUS = 0.06;             // Max radius in normalized coords (0.5 = 1/4 screen height)
+const float MAX_RADIUS = 0.09;             // Max radius in normalized coords (0.5 = 1/4 screen height)
 const float ANIMATION_START_OFFSET = 0.0;        // Start the ripple slightly progressed (0.0 - 1.0)
-vec4 COLOR = vec4(0.35, 0.36, 0.44, 1.0); // change to iCurrentCursorColor for your cursor's color
+vec4 COLOR = vec4(1.00, 0.88, 0.21, 1.0); // change to iCurrentCursorColor for your cursor's color
+// vec4 COLOR = vec4(0.35, 0.36, 0.44, 1.0); // change to iCurrentCursorColor for your cursor's color
 const float CURSOR_WIDTH_CHANGE_THRESHOLD = 0.5; // Triggers ripple if cursor width changes by this fraction
-const float BLUR = 3.0;                    // Blur level in pixels
+const float BLUR = 3.5;                    // Blur level in pixels
 
 
 // Easing functions
