a2b2bf22ba467452d4285b1879041bf63b26d40d
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-01 15:55:45 +0530

    Updated the tmux config files location

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -2,9 +2,8 @@
   home.file.".config/zellij".source = ./config_local_folder/zellij;
   home.file.".config/fastfetch".source = ./config_local_folder/fastfetch;
   home.file.".config/MangoHud".source = ./config_local_folder/MangoHud;
+  home.file.".config/tmux".source = ./config_local_folder/tmux;
   home.file.".config/ghostty".source = ./config_local_folder/ghostty;
   home.file.".config/gamemode.ini".source = ./config_local_folder/gamemode.ini;
-
-  # x
 }
 
