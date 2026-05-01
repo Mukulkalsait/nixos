3da6fae77dbd2a9c1b5cb36815981d17e4964eb8
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-02 02:38:41 +0530

    Tmux conf

--- hosts/system_apps/envVariables.nix
+++ hosts/system_apps/envVariables.nix
@@ -16,9 +16,7 @@
 
   # This Provides the defualt tmux env for both root and non root users. 
   # making seemlaes neovim + tmux setup possible in  root env
-  security.sudo.extraConfig = ''
-    Defaults env_keep += "TMUX TMUX_PANE XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME"
-  '';
+  security.sudo.extraConfig = '' Defaults env_keep += "TMUX TMUX_PANE XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME" '';
 }
 
 # ❯ ls $(nix eval --raw nixpkgs#glibc.dev)/include/stdio.h
