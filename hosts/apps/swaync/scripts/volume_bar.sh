a3043affff2df0b194ba4eba1107b8b07e98afdf
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-02 19:16:48 +0530

    volume bar updated

--- hosts/apps/swaync/scripts/volume_bar.sh
+++ hosts/apps/swaync/scripts/volume_bar.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
-# vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
-# muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o MUTED)
+vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
+muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o MUTED)
 
-if [ "$value" -eq 0 ]; then
+if [[ "$muted" == "MUTED" ]]; then
   icon="🔇"
-elif [ "$value" -lt 30 ]; then
-  icon="🔈"
-elif [ "$value" -lt 70 ]; then
-  icon="🔉"
+  value=0
 else
-  icon="🔊"
+  value=$vol
+  if [ "$value" -lt 30 ]; then
+    icon="🔈"
+  elif [ "$value" -lt 70 ]; then
+    icon="🔉"
+  else
+    icon="🔊"
+  fi
 fi
 
 notify-send \
   -h int:value:"$value" \
   -h string:x-canonical-private-synchronous:volume \
-  "$icon Volume  ${value}%"
+  "$icon  Volume ${value}%"
