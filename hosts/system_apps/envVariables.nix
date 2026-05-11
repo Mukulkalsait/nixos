295937b07a2899ffbcc3cc0628755db5cef07888
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-11 17:11:04 +0530

    Env Cleaned rustpu to fenix

--- hosts/system_apps/envVariables.nix
+++ hosts/system_apps/envVariables.nix
@@ -1,11 +1,6 @@
 # Y: envornment varialbes fro all session.
 { pkgs, ... }: {
   environment.variables = {
-    # Y: USE THIS TEMPERORYLY
-    # export LIBCLANG_PATH=$(nix eval --raw nixpkgs#llvmPackages.libclang.lib)/lib
-    # export C_INCLUDE_PATH=$(nix eval --raw nixpkgs#glibc.dev)/include
-    # # LD = "${pkgs.lld}/bin/ld.lld";
-    # RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/library";
 
     # Languages
     CC = "${pkgs.gcc}/bin/gcc";
