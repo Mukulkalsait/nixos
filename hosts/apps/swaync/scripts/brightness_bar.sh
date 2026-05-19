385cf273790bbe313dc43927a228cc6e0ecea71e
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-20 01:21:41 +0530

    Updated costume notifican time small

--- hosts/apps/swaync/scripts/brightness_bar.sh
+++ hosts/apps/swaync/scripts/brightness_bar.sh
@@ -6,6 +6,7 @@ percent=$((brightness * 100 / max))
 
 notify-send \
   -a "brightness" \
+  -t 2200 \
   -h int:value:"$percent" \
   -h string:x-canonical-private-synchronous:brightness \
   "🔆 Brightness ${percent}%"
