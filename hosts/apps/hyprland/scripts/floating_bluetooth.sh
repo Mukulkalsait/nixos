65a75a219cc2ebbb1afb9321b11cb81169ac4601
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-22 17:04:23 +0530

    Updates

--- hosts/apps/hyprland/scripts/floating_bluetooth.sh
+++ hosts/apps/hyprland/scripts/floating_bluetooth.sh
@@ -2,7 +2,7 @@
 
 # If impala exists → kill it
 if hyprctl clients | grep -q "class: bluetooth"; then
-  hyprctl dispatch closewindow class:bluetuith || pkill -f "kitty --class=bluetuith"
+  hyprctl dispatch closewindow class:bluetooth || pkill -f "kitty --class=bluetooth"
 else
   kitty --class="bluetooth" --title="BlueTuiTh 🎧" -e bluetuith &
   sleep 0.1
