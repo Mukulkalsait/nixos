2cb0c08903fbe181684b7fcb5cc73b8c7e3da49a
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-06-04 19:48:54 +0530

    Updates

--- hosts/apps/hyprland/hypridle.nix
+++ hosts/apps/hyprland/hypridle.nix
@@ -1,12 +1,46 @@
+# {
+#   services.hypridle = {
+#     enable = true;
+#     settings = {
+#       general = {
+#         before_sleep_cmd = "loginctl lock-session";
+#         after_sleep_cmd = "hyprctl dispatch dpms on";
+#         ignore_dbus_inhibit = false;
+#         lock_cmd = "pidof hyprlock || hyprlock";
+#       };
+#
+#       listener = [
+#         {
+#           timeout = 180;
+#           on-timeout = "brightnessctl -s set 30";
+#           on-resume = "brightnessctl -r";
+#         }
+#         {
+#           timeout = 300;
+#           on-timeout = "loginctl lock-session";
+#         }
+#         {
+#           timeout = 600;
+#           on-timeout = "hyprctl dispatch dpms off";
+#           on-resume = "hyprctl dispatch dpms on";
+#         }
+#         {
+#           timeout = 1200;
+#           on-timeout = "sysemctl suspend";
+#         }
+#       ];
+#     };
+#   };
+# }
 {
   services.hypridle = {
     enable = true;
     settings = {
       general = {
-        before_sleep_cmd = "loginctl lock-session";
+        before_sleep_cmd = "dms ipc call lock lock";
         after_sleep_cmd = "hyprctl dispatch dpms on";
         ignore_dbus_inhibit = false;
-        lock_cmd = "pidof hyprlock || hyprlock";
+        lock_cmd = "dms ipc call lock lock";
       };
 
       listener = [
@@ -17,7 +51,7 @@
         }
         {
           timeout = 300;
-          on-timeout = "loginctl lock-session";
+          on-timeout = "dms ipc call lock lock";
         }
         {
           timeout = 600;
@@ -26,7 +60,7 @@
         }
         {
           timeout = 1200;
-          on-timeout = "sysemctl suspend";
+          on-timeout = "systemctl suspend"; # Fixed typo: sysemctl → systemctl
         }
       ];
     };
--- hosts/apps/hyprland/scripts/avatarchange.sh
+++ hosts/apps/hyprland/scripts/avatarchange.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 # Set the avatar using DBUS
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
 dms ipc call profile setImage /home/mukuldk/1_file/9_Pictures/avatar/YangKai-Square.png
 
 echo "Done! Check if avatar appears in DMS"
