b3d1637c6498017a81f18a64568620603612ed2f
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-07-25 12:50:41 +0530

    Greeted set proper hyprland command

--- hosts/system_apps/greetd.nix
+++ hosts/system_apps/greetd.nix
@@ -1,27 +1,14 @@
-# Y: TUI Login manager.
-#
-# { config, pkgs, ... }: {
-#
-#   services.greetd = {
-#     enable = true;
-#     settings = {
-#       default_session = {
-#         command =
-#           "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd Hyprland";
-#         user = "greeter";
-#       };
-#     };
-#   };
-# }
+# greetd.nix
 
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd ${pkgs.hyprland}/bin/Hyprland";
+        # command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd ${pkgs.hyprland}/bin/Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --greeting '☣ Danger : PredatorNix ☣' --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd ${config.programs.hyprland.package}/bin/Hyprland";
         user = "greeter";
       };
     };
