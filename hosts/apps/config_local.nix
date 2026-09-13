634acd0a78a2e80bb628cd7c3bcc02c8979bf61f
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-09-14 03:39:32 +0530

    feat: moved configs into nix

--- hosts/apps/config_local.nix
+++ hosts/apps/config_local.nix
@@ -1,11 +1,12 @@
 {
+  home.file.".config/clippy".source = ./config_local_folder/clippy;
   home.file.".config/fastfetch".source = ./config_local_folder/fastfetch;
   home.file.".config/gamemode.ini".source = ./config_local_folder/gamemode.ini;
   home.file.".config/ghostty".source = ./config_local_folder/ghostty;
-  home.file.".config/rustfmt".source = ./config_local_folder/rustfmt;
+  home.file.".config/glow".source = ./config_local_folder/glow;
   home.file.".config/MangoHud".source = ./config_local_folder/MangoHud;
+  home.file.".config/rustfmt".source = ./config_local_folder/rustfmt;
   home.file.".config/tmux".source = ./config_local_folder/tmux;
   home.file.".config/zellij".source = ./config_local_folder/zellij;
-  home.file.".config/clippy".source = ./config_local_folder/clippy;
 }
 
