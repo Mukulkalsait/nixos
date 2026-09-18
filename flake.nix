4188d847d14afdc50cb8981f3630fea3b8a24883
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-09-18 16:18:28 +0530

    docs: ghostty cachix aded

--- flake.nix
+++ flake.nix
@@ -93,17 +93,17 @@
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://cache.nixos-cuda.org"
-      "https://ghostty.cachix.org"
       "https://zen-browser.cachix.org"
       "https://noctalia.cachix.org"
+      "https://ghostty.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-      "ghostty.cachix.org-1:QB1qQUdfhD6tdmclusG3N942jgWnMspJu1c4H05JnwY="
       "zen-browser.cachix.org-1:7W420S/c6Fj6S63bA78q2f754M8d14t0t+p4G73w2E8="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
     ];
   };
 
