cf32f70f557304a78a806ca729a0089b78ed0017
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-09-18 16:21:28 +0530

    refactor: Kitty + movement

--- hosts/apps/kitty.nix
+++ hosts/apps/kitty.nix
@@ -1,64 +1,87 @@
-# Y: kitty.nix
+{ ... }:
 
-{ ... }: {
+{
 
-  # Kitty
   programs.kitty = {
     enable = true;
+
+
     # themeFile = "Bright_Lights";
     # themeFile = "Encom";
     # themeFile = "Lavandula";
-    themeFile = "Argonaut";
+    # themeFile = "Argonaut";
+    themeFile = "TokyoNight";
+    # R: use "kitty +kitten themes"
+
     font = {
-      size = 9;
-      # ======= FONT==============
-      # name = "JetBrainsMono Nerd Font";
-      #set to name = "Go Mono for Powerline";
-      # name = "Liberation Mono";
-      name = "Hack";
-      # name = "Cousine for Powerline";
-      # name = "Meslo LG M for Powerline";
-      # name = "Go Mono for Powerline";
-      # name-bold = "DejaVu Sans Mono";
+      size = 9; # ghostty: font-size = 9
+      name = "JetBrainsMono Nerd Font"; # ghostty: font-family (primary)
     };
+
     settings = {
-      # background = "#000000";
-      background_opacity = "0.70";
-      confirm_os_window_close = 0;
-      # cursor_shape = "beam";
-      cursor_trail = "5";
-      cursor_trail_decay = "0.20 0.5"; # X and Y decay
+
+      # bold_font = "MonaspiceKr Nerd Font"; # ghostty: font-family-bold - this line was missing in your last version, which is why bold text wasn't rendering distinctly
+      # italic_font = "MonaspiceRn Nerd Font"; # ghostty: font-family-italic
+      # bold_italic_font = "MonaspiceKr Nerd Font"; # ghostty: font-family-bold-italic
+
+      bold_font = "VictorMono Nerd Font";
+      italic_font = "VictorMono Nerd Font"; # ghostty: font-family-italic
+      bold_italic_font = "VictorMono Nerd Font"; # ghostty: font-family-bold-italic
+
+      font_features = "JetBrainsMono-Regular +liga +dlig";
+
+      # ---- Window / chrome ----
+      background_opacity = "0.75"; # ghostty: background-opacity = 0.75
+      confirm_os_window_close = "0"; # ghostty: confirm-close-surface = false
+      window_padding_width = "0"; # ghostty: window-padding-y/x = 0
+      hide_window_decorations = "yes"; # keep the chrome-less look you already run
+
+      # ---- Colors ----
+      # theme provides colors 0-15, these override background/foreground/
+      # cursor on top of it - same pattern as ghostty's theme + overrides.
+      background = "#000000"; # ghostty: background = #000000
+      foreground = "#c8d3f5"; # ghostty: foreground = #c8d3f5
+      # cursor = "#c8d3f5";
+
+      # ---- Shell startup ----
+      # ghostty: command = zsh -ic "fastfetch; exec zsh"
+      shell = "zsh -ic \"fastfetch; exec zsh\"";
+
+      # ---- Clipboard ----
+      # kitty disables clipboard *read* by default for security. This
+      # opts back in to match ghostty's clipboard-read/write = allow.
+      clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
+
+      # ---- Cursor ----
+      # cursor_shape = "block"; # trail reads cleanest on a block cursor
+      cursor_shape = "beam";
+      cursor_beam_thickness = "1.5";
+      cursor_underline_thickness = "2.0";
+
+      cursor_blink_interval = "0";
+      cursor_stop_blinking_after = "0";
+
+      # ---- Cursor trail ----
+      cursor_trail_start_threshold = "0";
+
+      # cursor_trail = "20";
+      # cursor_trail_decay = "0.1 0.3";
+      # cursor_trail_thickness = "1.2 1.2";
+
+      cursor_trail = "1";
+      cursor_trail_decay = "0.01 0.5"; # X and Y decay
       cursor_trail_thickness = "1.2 1.2"; # X and Y thickness
+
       # Y: faster
       # cursor_trail = "6"; # number of ghost cursors
       # cursor_trail_decay = "0.20 0.08"; # fast fade in X/Y directions
       # cursor_trail_thickness = "1.2 1.2"; # default thickness
-
-      # Ghostty settings:
-      # vec4 TRAIL_COLOR = iCurrentCursorColor; // can change to eg: vec4(0.2, 0.6, 1.0, 0.5);
-      # const float DURATION = 0.09; // in seconds
-      # const float MAX_TRAIL_LENGTH = 0.3;
-      # const float THRESHOLD_MIN_DISTANCE = 1.5; // min distance to show trail (units of cursor width)
-      # const float BLUR = 2.0; // blur size in pixels (for antialiasing)
-      #
-      # // --- CONSTANTS for easing functions ---
-      # const float PI = 3.14159265359;
-      # const float C1_BACK = 1.70158;
-      # const float C2_BACK = C1_BACK * 1.525;
-      # const float C3_BACK = C1_BACK + 1.0;
-      # const float C4_ELASTIC = (2.0 * PI) / 3.0;
-      # const float C5_ELASTIC = (2.0 * PI) / 4.5;
-      # const float SPRING_STIFFNESS = 9.0;
-      # const float SPRING_DAMPING = 0.9;
-      cursor_blink_interval = "0"; # disables blinking completely
     };
+
     keybindings = {
       "ctrl+shift+c" = "copy_to_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
       "ctrl+shift+q" = "no_op"; # unbind close_tab
     };
   };
-
-
-
 }
