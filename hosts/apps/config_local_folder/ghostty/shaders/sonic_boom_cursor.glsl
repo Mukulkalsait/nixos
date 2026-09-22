10ad6928d30271eb22dad33010fa6f660354aa3b
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-09-22 19:20:44 +0530

    chore: ghostty shreaders modification.

--- hosts/apps/config_local_folder/ghostty/shaders/sonic_boom_cursor.glsl
+++ hosts/apps/config_local_folder/ghostty/shaders/sonic_boom_cursor.glsl
@@ -1,6 +1,6 @@
 // CONFIGURATION
 const float DURATION = 0.100;               // How long the ripple animates (seconds)
-const float MAX_RADIUS = 0.09;             // Max radius in normalized coords (0.5 = 1/4 screen height)
+const float MAX_RADIUS = 0.1;             // Max radius in normalized coords (0.5 = 1/4 screen height)
 const float ANIMATION_START_OFFSET = 0.0;        // Start the ripple slightly progressed (0.0 - 1.0)
 vec4 COLOR = vec4(1.00, 0.88, 0.21, 1.0); // change to iCurrentCursorColor for your cursor's color
 // vec4 COLOR = vec4(0.35, 0.36, 0.44, 1.0); // change to iCurrentCursorColor for your cursor's color
